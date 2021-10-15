947695a91861a6a8528d2039e2af0e6fa2658fc9
DanielReinagel <daniel.e.reinagel@gmail.com>
2021-10-15 15:29:16 -0700

    view component testing running as intended

--- src/components/View.test.js
+++ src/components/View.test.js
@@ -2,11 +2,33 @@ import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import View from './View';
 
+import articleService from '../services/articleServices';
+jest.mock('../services/articleServices');
+
+const mockArticles = num => {
+  const output = []
+  for(let i=0; i<num; i++){
+    output.push({
+      headline: `headline${i}`,
+      author: `author${i}`,
+      summary: `summary${i}`,
+      body: `body${i}`,
+      image: i,
+      id: `abcde${i}`
+    })
+  }
+  return output;
+}
+
 test("renders zero articles without errors", async () => {
+  articleService.mockResolvedValueOnce({data:mockArticles(0)})
+  render(<View/>);
 });
 
-// test("renders three articles without errors", async ()=> {
-// });
+test("renders three articles without errors", async ()=> {
+  articleService.mockResolvedValueOnce({data:mockArticles(3)})
+  render(<View/>);
+});
 
 //Task List
 //1. Complete the above two tests. Make sure to mocking the articleService call before rendering.
